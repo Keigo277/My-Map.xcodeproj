6e99a62af9511ccd4e516b0e0b21f016cf4868df
柳沢圭吾 <83903345+Keigo277@users.noreply.github.com>
2021-05-10 10:18:40 +0900

    Add files via upload

---
+++ MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 柳沢圭吾 on 2021/02/20.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
